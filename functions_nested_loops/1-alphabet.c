ed438b480aa95ff20a37107762b4217a82d24113
Babadu31 <bastienmiss31@gmail.com>
2022-06-09 10:04:09 -0700

    1-alphabet.c-1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *
+ * print_alphabet - vérifier la description 
+ * Description: imprimer l'alphabet en minuscule et suivi d'un saut à la ligne
+ * Return: rien.
+ */
+void print_alphabet(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z' ; i++)
+_putchar(i);
+_putchar('\n');
+}
