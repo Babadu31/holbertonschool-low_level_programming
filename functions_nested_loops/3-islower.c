c6b4d3cb6b96fe144ef86365878a329e51c8f052
Babadu31 <bastienmiss31@gmail.com>
2022-06-10 00:49:19 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - verifier main
+ * @c: caractère d'entrée
+ * Description: la fonction utilise _putchar fonction pour imprimer
+ * alphabet en minuscule imprimer 10 fois
+ * Return: 1 si c'est en minuscule et 0 si c'est en majusculle
+ */
+int _islower(int c)
+{
+char i;
+int lower = 0;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i == c)
+lower = 1;
+}
+
+return (lower);
+}
