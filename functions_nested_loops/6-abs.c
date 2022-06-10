be9d5264336c463ac9895805b8fa6de9c7cf4c7d
Babadu31 <bastienmiss31@gmail.com>
2022-06-10 04:54:21 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - point d'entré
+ * @n: int code va vérifier pour ce charactère
+ *
+ * Return: la valeur absolue de l'intégreur
+ */
+
+int _abs(int n)
+{
+if (n <= -1)
+{
+n *= -1;
+}
+return (n);
+}
