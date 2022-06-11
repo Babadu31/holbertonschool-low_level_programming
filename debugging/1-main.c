3ceb44f871e46519acad0caf06c5c73f4381082f
Babadu31 <bastienmiss31@gmail.com>
2022-06-11 10:13:17 -0700

    11/06/22-19:13-1-main.c

---
+++ debugging/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - teste la fonction qui imprime si l'entier est positif ou négatif
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+
+positive_or_negative(i);
+return (0);
+}
