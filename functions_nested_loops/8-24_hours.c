1e06f9c71314b8a47380dca8a020d60972a0bfed
Babadu31 <bastienmiss31@gmail.com>
2022-06-11 04:02:25 -0700

    11/06/22-13/02-8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - point d'entré
+ *
+ * Return: annulation réussie, impressions min
+ */
+void jack_bauer(void)
+{
+int main, hr;
+
+for (hr = 0; min <= 23; hr++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar((min % 10) + '0');
+_putchar('\n');
+}
+}
+}
