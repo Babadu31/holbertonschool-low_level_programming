57f597d1aba86ba44fe1394d4f3406ffcdc50711
Babadu31 <bastienmiss31@gmail.com>
2022-06-11 01:04:31 -0700

    11/06/22-10/04

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit -- point d'entrée
+ * @n: intégreur
+ * Return: valeur du dernier
+ */
+int print_last_digit(int n)
+{
+int last = n % 10;
+
+if (last < 0)
+{
+last *= -1;
+}
+_putchar(last + '0');
+return (last);
+}
