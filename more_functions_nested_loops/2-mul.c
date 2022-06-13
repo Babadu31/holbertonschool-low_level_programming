1c2043e4d5c30cda1fda20162be447aa4f464422
Babadu31 <bastienmiss31@gmail.com>
2022-06-13 02:31:42 -0700

    13/06/22-11:31-2-mul.c

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,9 +2,9 @@
 
 /**
  * mul - multiplie deux entiers
- * @a : int à multiplier par b
- * @b : int à multiplier par a
- * Return : le résultat de l'opération
+ * @a: int à multiplier par b
+ * @b: int à multiplier par a
+ * Return: le résultat de l'opération
  */
 
 int mul(int a, int b)
