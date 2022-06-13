302f46e3f819f5e342a988a89119e1b33ce10337
Babadu31 <bastienmiss31@gmail.com>
2022-06-13 07:50:48 -0700

    13/06/22-16:50-7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+void print_diagonal(int n)
+{
+  if (n <= 0)
+    {
+      _putchar('\n');
+    }
+  else
+    {
+      
--- more_functions_nested_loops/_putchar.c
+++ more_functions_nested_loops/_putchar.c
@@ -1,1 +1,16 @@
-# include "main.h"
+#include "main.h"
+#include <unistd.h>
+
+
+/**
+ * _putchar - écrit le caractère c sur stdout
+ * @c: Le caractère à imprimer
+ *
+ * Return: En cas de succès 1.
+ * En cas d'erreur, -1 est renvoyé et errno est défini de manière appropriée.
+ */
+
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
+}
