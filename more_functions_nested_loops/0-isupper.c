330e212674ea899b2be8a9f689eef7139fedd77b
Babadu31 <bastienmiss31@gmail.com>
2022-06-13 01:59:58 -0700

    13/06/22-10:59-0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - vérifie le caractères majuscule.
+ * @c: le charactère à vérifier
+ * Return: 1 si c est en majuscule, 0 sinon
+ */
+
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
