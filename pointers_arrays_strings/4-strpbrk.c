75b1dc845a73e821cd1e9beed929b75dcff898af
Babadu31 <bastienmiss31@gmail.com>
2022-06-21 06:21:37 -0700

    21/06/22-15:21-4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,14 +5,13 @@
  * _strpbrk - recherche dans une chaine l'un des ensembles d'octets
  * @s: chaîne à rechercher
  * @accept: chaîne contenant les octets à rechercher
- * Return: pointeur vers l'octet de s qui correspond à l'un des octets de accept 
+ * Return: pointeur vers l'octet de s
+ * qui correspond à l'un des octets de accept
  * ou NULL si aucun octet
  */
-
 char *_strpbrk(char *s, char *accept)
 {
 int x, y;
-
 for (x = 0; *s != '\0'; x++)
 {
 for (y = 0; accept[y] != '\0'; y++)
