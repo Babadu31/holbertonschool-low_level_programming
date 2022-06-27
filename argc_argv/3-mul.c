2df575cc4a4527256f64830a9d5b83cc4180590f
Babadu31 <bastienmiss31@gmail.com>
2022-06-27 06:07:05 -0700

    27/06/22-15:06-3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplie deux chiffres
+ * @argc: nombres d'arguments passé dans la fonction
+ * @argv: argument vecteur du pointeur
+ * Return: 0 si pas d'erreurs, 1 pour le reste
+ */
+
+int main(int argc, char *argv[])
+{
+int a, b, c;
+
+if (argc != 3)
+{
+puts("Error");
+return (1);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+c = a * b;
+printf("%d\n", c);
+return (0);
+}
