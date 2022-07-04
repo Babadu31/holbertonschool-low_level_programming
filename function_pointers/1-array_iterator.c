dd186145a8a7fdcb824ffa7ba033c697b28f415e
Babadu31 <bastienmiss31@gmail.com>
2022-07-04 13:17:23 -0700

    04/07/22-22:17-1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -10,7 +10,12 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t x;
+unsigned int x;
+
+if (array == NULL)
+return;
+if (action == NULL)
+return;
 
 for (x = 0; x < size; x++)
 action(array[x]);
